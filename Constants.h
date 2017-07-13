c0b80cff31b9084223e931a059804d2d9d6cc5c8
deboraac <deby.ac@gmail.com>
2017-07-13 12:30:27 -0300

    Versão com o refactoring - 13/07/2017

---
+++ Constants.h
@@ -0,0 +1,24 @@
+#define BUZZER 15
+
+#define WAITING_START   	0 
+#define PLAYING_SEQ     	1
+#define USER_TYPING     	3
+#define VALIDATING_SEQ  	4
+#define WAITING_EASTER_EGG 	5
+
+#define BTN_WHITE    				11
+#define BTN_RED  				10
+#define BTN_BLUE   				9
+#define BTN_PINK   				8
+#define BTN_START    			3
+
+#define L_RED    	7
+#define L_WHITE 	6
+#define L_BLUE  	5
+#define L_PINK  	4
+
+#define DEBUGGING 0
+
+const int BOTOES_SEQ[]={BTN_START,BTN_WHITE,BTN_RED,BTN_BLUE,BTN_PINK};
+
+const int EASTER_EGG_SEQ[] = { BTN_PINK, BTN_PINK, BTN_RED, BTN_RED, BTN_WHITE, BTN_BLUE, BTN_WHITE, BTN_BLUE };
---
+++ Genius.h
@@ -0,0 +1,378 @@
+#include <C:/Users/garagebh/Documents/WomenBH/genius-refactoring/Notes.h>
+#include <C:/Users/garagebh/Documents/WomenBH/genius-refactoring/Constants.h>
+
+int interval = 500;
+int noSound = 200;
+int seq[50];
+int posSeq = 0;
+int posType = 0;
+int state = WAITING_EASTER_EGG; 
+int key = 0; 
+int running = 0;
+int startValue = 0;
+int easterEggPos = 0;
+
+void setup() 
+{                
+  pinMode(L_RED, OUTPUT);     
+  pinMode(L_WHITE, OUTPUT);     
+  pinMode(L_BLUE, OUTPUT);     
+  pinMode(L_PINK, OUTPUT);
+  pinMode(BTN_WHITE, INPUT); 
+  pinMode(BTN_PINK, INPUT);      
+  pinMode(BTN_BLUE, INPUT);    
+  pinMode(BTN_RED, INPUT);    
+  pinMode(BTN_START, INPUT);      
+  noSound = 0;
+
+  playStart();
+  noSound = 50;
+  Serial.begin(9600); 
+  randomSeed(analogRead(0));
+  generateSeq();
+}
+
+void startingDelay(){
+    Serial.print("startingDelay");  
+
+    delay(500);
+    
+    //Acende todas as luzes
+    for (int thisLight = L_PINK; thisLight <= L_RED; thisLight++) {
+      digitalWrite(thisLight, HIGH);  
+    }
+    delay(500);
+    //Apaga todas as luzes
+    for (int thisLight = L_PINK; thisLight <= L_RED; thisLight++) {
+      digitalWrite(thisLight, LOW);  
+    }
+    delay(1000);
+}
+int botao = -1;
+
+void loop() {
+    
+    running = 1;
+    switch(state)
+    {
+      case WAITING_EASTER_EGG:
+        if (DEBUGGING){
+          Serial.print("WAITING_EASTER_EGG ");  
+          Serial.println(WAITING_EASTER_EGG);
+        }
+
+        botao = verificaBotao(true);
+        
+        if (botao!=0){
+          Serial.print("botao apertado: ");
+          Serial.println(botao);
+          if (botao==BTN_START){
+             state=PLAYING_SEQ;
+             startingDelay();
+             return;
+          }
+          else{
+            if (validaEasterEgg (botao)){
+              Serial.print("easterEggPos ");
+              Serial.println(easterEggPos);
+              delay(500);
+              easterEggPos++;
+            }
+            else{
+              easterEggPos=0;
+            }
+            if(easterEggPos==8){
+              easterEggPos=0;
+              playEasterEgg();
+            }
+          }
+          botao = 0;
+        }
+       
+      return;
+      case WAITING_START:   
+        interval = 500;
+        if (DEBUGGING){
+          Serial.print("WAITING_START");  
+          Serial.println(WAITING_START);
+        }
+        key = pushStart();
+        if(key == 1){
+          state = PLAYING_SEQ;
+          startingDelay();
+        }
+        return;  
+      case PLAYING_SEQ:
+        if (DEBUGGING)
+          Serial.println("PLAYING_SEQ");  
+        playSeq();
+        posType = 0;
+        state = USER_TYPING;
+        return;
+      case USER_TYPING:
+        if (DEBUGGING)
+          Serial.println("USER_TYPING");  
+        key = 1;
+        int ok = true;
+        while(ok)
+        {
+            key = readKey(true);
+            if (key == 0)
+              break;
+            play(key-4);
+            ok = validateSeq(key);
+            if (DEBUGGING)
+            {
+              Serial.print("ok=");
+              Serial.println(ok);
+            }
+            if (ok)
+            {
+              if (posType == posSeq)
+              {
+                delay(1000);
+                posSeq++;
+                state = PLAYING_SEQ;
+                interval = interval * 0.95;
+                return;
+              }  
+              else
+              {
+                posType++;
+              }            
+            }
+        } 
+        playWrong();
+        Serial.print("Voce fez ");
+        Serial.println(posSeq);
+        if (posSeq == 0)
+          Serial.println("Putz, como vc e burro!");
+        else
+           if (posSeq <= 5)
+              Serial.println("Alzheimer");
+           else
+              if (posSeq < 10)
+                 Serial.println("Uai... ate que nao tao pessimo");
+              else
+                 if (posSeq < 15)
+                   Serial.println("Uia!");
+                 else
+                   Serial.println("Cool!");
+        generateSeq();
+        state = WAITING_EASTER_EGG;
+        return;
+          
+  }
+}
+
+int pushStart(){
+  startValue = digitalRead(BTN_START);
+
+  if (startValue == 1){
+    Serial.print("StartValue=");
+    Serial.println(startValue);
+    return 1;
+  }
+  
+  return 0;
+}
+
+int validateSeq(int key)
+{
+  
+  return (key-4) == seq[posType]; // (key -4) = key to LED
+}
+
+int readKey(int validateMilis)
+{
+   unsigned long ms = millis();
+   int value = LOW;
+   while(value == LOW)
+   {
+     for (int i = BTN_WHITE; i >= BTN_PINK; i--)
+     {
+       if (validateMilis)
+         if ((millis() - ms) > 3000)
+         {
+           if (DEBUGGING)
+             Serial.println("Timeout"); 
+           return 0;
+         }
+       value = digitalRead(i);
+       if (value == HIGH)
+       {
+         return i;
+       }
+     }
+  }
+}
+
+
+int verificaBotao(int validateMilis)
+{
+   unsigned long ms = millis();
+   int value = LOW;
+   while(value == LOW)
+   {
+     for (int i = 5; i >= 0; i--)
+     {
+       if (validateMilis)
+         if ((millis() - ms) > 3000)
+         {
+           if (DEBUGGING)
+             Serial.println("Timeout"); 
+           return 0;
+         }
+       value = digitalRead(BOTOES_SEQ[i]);
+       
+       if (value == HIGH)
+       {
+         return BOTOES_SEQ[i];
+       }
+     }
+  }
+  return 0;
+}
+
+void playPich(int pich, int duration)
+{
+  tone(BUZZER, pich);
+  delay(duration);
+  noTone(BUZZER);
+}
+
+void playStart() 
+{
+    int light = L_PINK;
+    for (int thisNote = 1; thisNote < (LIFE[0] * 2 + 1); thisNote = thisNote + 2) { // Run through the notes one at a time
+      digitalWrite(light, HIGH);  
+      tone(BUZZER, LIFE[thisNote], (1000/LIFE[thisNote + 1]));        // Play the single note
+      delay((1000/LIFE[thisNote + 1]) * 1.30);                        // Delay for the specified time
+      noTone(BUZZER);                                                 // Silence the note that was playing
+
+      digitalWrite(light, LOW);  
+      if (light == L_RED) {
+        light = L_PINK;
+      } else {
+        light++;
+      }
+    }
+    delay(500);
+}
+
+void playWrong()
+{
+    Serial.println("playWrong");
+    running = 0;
+    int light = L_PINK;
+    for (int thisNote = 1; thisNote < (DEATH[0] * 2 + 1); thisNote = thisNote + 2) { // Run through the notes one at a time
+      if (DEATH[thisNote] != NOTE_H) {
+        digitalWrite(light, HIGH);
+      }
+      tone(BUZZER, DEATH[thisNote], (1000/DEATH[thisNote + 1]));// Play the single note
+      delay((1000/DEATH[thisNote + 1]) * 1.30);                        // Delay for the specified time
+      noTone(BUZZER);                                                 // Silence the note that was playing
+
+     digitalWrite(light, LOW);  
+      if (light == L_RED) {
+        light = L_PINK;
+      } else if (DEATH[thisNote] != NOTE_H) {
+        light++;
+      }
+    }
+      delay(500);
+}
+
+void playEasterEgg() 
+{
+    int light = L_PINK;
+    int x = 0;
+    while (NOTES_MISS_IMP[x]!=-1){
+       if (NOTES_MISS_IMP[x] != NOTE_H) {
+        digitalWrite(light, HIGH);
+       }
+       tone(BUZZER, NOTES_MISS_IMP[x], (TIME_NOTES_MISS_IMP[x]));
+       delay(TIME_NOTES_MISS_IMP[x]);
+       noTone(BUZZER);
+       digitalWrite(light, LOW);  
+       if (light == L_RED) {
+          light = L_PINK;
+       } else {
+          light++;
+       }
+       x++;
+    }
+    delay(500);
+}
+
+void demo()
+{
+  interval = 500;
+  while(interval > 5)
+  {
+    play(L_RED);
+    play(L_WHITE);
+    play(L_BLUE);
+    play(L_PINK);
+    interval = interval / 1.5;
+  }
+}
+
+void blink(int led, int pich)
+{
+  digitalWrite(led, HIGH);
+  tone(BUZZER,pich);
+  delay(interval);
+  digitalWrite(led, LOW);
+  noTone(BUZZER);
+  delay(noSound);
+}
+
+void play(int pich)
+{
+  switch(pich)
+  {
+    case 7:
+      blink(L_RED,NOTE_C4);
+      break;
+    case 6:
+      blink(L_WHITE,NOTE_E4);
+      break;
+    case 5:
+      blink(L_BLUE,NOTE_G4);
+      break;
+    case 4:
+      blink(L_PINK,NOTE_AS4);
+      break;
+  }  
+}
+
+void generateSeq()
+{
+  posSeq = 0;
+  if (DEBUGGING)
+    Serial.println("generateSeq");
+  int note;
+  for (int i = 0; i < 50; i++)
+  {
+    seq[i] = random(4) + 4;
+    if (DEBUGGING)
+      Serial.println(seq[i]);
+  }
+}
+
+void playSeq()
+{
+  for (int i = 0; i <= posSeq; i++)
+  {
+    play(seq[i]);
+  }
+}
+int validaEasterEgg(int proxKeyEasterEgg){
+   if (proxKeyEasterEgg == EASTER_EGG_SEQ[easterEggPos]){  
+    Serial.print("proxKeyEasterEgg=");
+    Serial.println(proxKeyEasterEgg);
+    return 1;
+  }
+  return 0;
+}
